e18646ad831c5bd70eb3e63d63b135ba762ef618
raichiuai <richi.stobert@gmail.com>
2022-02-16 15:41:23 +0100

    Update Main.java

--- FOP-2022-Projekt-Student-master/src/main/java/projekt/Main.java
+++ FOP-2022-Projekt-Student-master/src/main/java/projekt/Main.java
@@ -1,9 +1,9 @@
 package projekt;
 
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("first example for GitHub");
+        System.out.println("ich teste das jetzt auch mal");
     }
 }
