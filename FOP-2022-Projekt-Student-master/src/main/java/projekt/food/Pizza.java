840062720aa4d9f9f406f5bd28e0a4a0487fe121
eneapane <eneapane1@gmail.com>
2022-02-16 07:09:20 -0800

    interface Pizza

---
+++ FOP-2022-Projekt-Student-master/src/main/java/projekt/food/Pizza.java
@@ -0,0 +1,25 @@
+package projekt.food;
+
+import java.util.function.DoubleUnaryOperator;
+
+public interface Pizza extends Saucable{
+    /**
+     * The definition of this method describes the diameter of the pizza
+     * @return the diameter in cm of the actual Pizza type
+     */
+    double getDiameter();
+    interface Config extends Food.Config
+    {
+        /**
+         * @see IceCream.Config flavor to understand how the implementation of this method should work
+         * This method takes parameter:
+         * @param op the operator interface whose functional method describes the change of the diameter of the pizza
+         */
+        void diameter(DoubleUnaryOperator op);
+        /**
+         * You may have inserted many unaryOperators, each describing a certain change. This method
+         * @return the composition of all these operations.
+         */
+        DoubleUnaryOperator getDiameterMutator();
+    }
+}
